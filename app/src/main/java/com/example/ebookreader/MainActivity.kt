21f70fe1dea983d397f5d1f5bdc5d5e634db74ac
Calvin Ku <calvin.j.ku@googlemail.com>
2021-10-04 17:34:30 +0800

    Databinding added.

--- app/src/main/java/com/example/ebookreader/MainActivity.kt
+++ app/src/main/java/com/example/ebookreader/MainActivity.kt
@@ -4,22 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.ebookreader.databinding.ActivityMainBinding
 import com.example.ebookreader.utils.TypeFaceStyler
 
 class MainActivity : AppCompatActivity() {
     val TAG = javaClass.simpleName
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Style UI
-        val loginDescription: TextView = findViewById(R.id.login_description)
+        val loginDescription = binding.loginDescription
         loginDescription.text =
             TypeFaceStyler(this, loginDescription)
             .styleText( "Kaliba", R.font.nunito_black)
             .styleText( "Reader", R.font.nunito_extralight_italic)
             .spannable
+
+        binding.invalidateAll()
     }
 }
